f6607df1ff8c829a29e68bfd24ed88a89b8f31ce
MarinDEV <mansakumarin@gmail.com>
2021-11-10 20:07:07 +0000

    Inside Ship Movement

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -6,15 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
-    public float speed = 500;
+    public float speed;
+    Vector2 mouseMovement;
 
-    void onMove(InputValue value) {
+    void OnMove(InputValue value) {
     	moveValue = value.Get<Vector2>();
+    	Cursor.lockState = CursorLockMode.Locked;
     }
 
     void FixedUpdate(){
-    	Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-    	Debug.Log(movement);
-    	GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+         Vector3 facingDirection = transform.forward;
+    	// Vector3 movement = new Vector3(moveValue.x * speed, 0.0f, moveValue.y * speed);
+        Vector3 movement = new Vector3(facingDirection.x * speed, 0.0f, facingDirection.z * speed);
+    	Vector3 newPos = (GetComponent<Rigidbody>().position + movement);
+    	GetComponent<Rigidbody>().position = newPos;
+   
+    	mouseMovement.x += Input.GetAxis("Mouse X")*12;
+        mouseMovement.y += Input.GetAxis("Mouse Y")*12;
+        transform.localRotation = Quaternion.Euler(0, mouseMovement.x,0);
     }
 }

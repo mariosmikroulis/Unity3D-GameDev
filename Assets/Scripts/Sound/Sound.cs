04e62f1435b0199fb27b76303b7efe0a44ba898c
xFrankyMxc <33936717+mariosmikroulis@users.noreply.github.com>
2022-01-14 05:28:50 +0000

    Finished the sound system. Controllable Master, SFX & Music Volumes

--- Assets/Scripts/Sound/Sound.cs
+++ Assets/Scripts/Sound/Sound.cs
@@ -5,7 +5,12 @@ using UnityEngine.Audio;
 public class Sound
 {
     public string name;
+
+    // Use either an Audio Clip or a gameObject to set the audio settings etc.
     public AudioClip clip;
+    public Transform custom;
+
+    public SoundType type = SoundType.Music;
 
     [Range(0f, 1f)]
     public float volume = 1f;
@@ -20,22 +25,21 @@ public class Sound
     [HideInInspector]
     public AudioSource source;
 
-    public void init(AudioSource _source) {
-        setSource(_source);
-        setClip(clip);
-
-        setVolume(volume);
-        setPitch(pitch);
-        setLoop(loop);
-        setMute(mute);
-    }
-
     public string getName() {
         return name;
     }
 
     public void setName(string _name) {
         name = _name;
+    }
+
+    public SoundType getType() {
+        return type;
+    }
+
+    public void setType(SoundType _type) {
+        type = _type;
+        setVolume(volume);
     }
 
     public AudioClip getClip() {
@@ -66,14 +70,25 @@ public class Sound
     }
 
     public void setVolume(float _volume) {
-        if(_volume < .0f) {
+        SoundManager master = SoundManager.getInstance();
+
+        if (_volume < .0f) {
             _volume = .0f;
         } else if(_volume > 1f) {
             _volume = 1f;
         }
 
         volume = _volume;
-        source.volume = _volume;
+
+        if (type == SoundType.Music) {
+            source.volume = _volume * master.getMasterSoundVolume() * master.getMusicSoundVolume();
+        } else if(type == SoundType.SFX) {
+            source.volume = _volume * master.getMasterSoundVolume() * master.getSFXSoundVolume();
+        }
+    }
+
+    public float getSourceVolume() {
+        return source.volume;
     }
 
     public float getVolume() {
@@ -82,11 +97,16 @@ public class Sound
 
     public void setMute(bool status) {
         mute = status;
-        source.mute = status;
+        source.mute = isSourceMuted();
     }
 
     public bool isMuted() {
         return mute == true;
+    }
+
+    public bool isSourceMuted() {
+        SoundManager master = SoundManager.getInstance();
+        return mute || master.isMasterSoundMuted() || type == SoundType.Music && master.isMusicSoundMuted() || type == SoundType.SFX && master.isSFXSoundMuted();
     }
 
     public void setLoop(bool _loop) {
@@ -104,6 +124,23 @@ public class Sound
 
     public void setSource(AudioSource _source) {
         source = _source;
+
+        setClip(clip);
+
+        setVolume(volume);
+        setType(type);
+        setPitch(pitch);
+        setLoop(loop);
+        setMute(mute);
+    }
+
+    public void setCustomSource() {
+        source = custom.GetComponent<AudioSource>();
+        clip = source.clip;
+        pitch = source.pitch;
+        loop = source.loop;
+        setVolume(source.volume);
+        setMute(source.mute);
     }
 
     public void playSound() {
---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,192 @@
+using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    // All global volume controllers.
+    public bool MuteMasterSound = false;
+    [Range(0f, 1f)]
+    public float MasterSoundVolume = 1f;
+
+    public bool MuteMusicSound = false;
+    [Range(0f, 1f)]
+    public float MusicSoundVolume = 1f;
+
+    public bool MuteSFXSound = false;
+    [Range(0f, 1f)]
+    public float SFXSoundVolume = 1f;
+
+
+    public Sound[] sounds;
+
+    public Sound[] soundsGameObjects;
+
+    public static SoundManager instance;
+
+    public static SoundManager getInstance() {
+        return instance;
+    }
+
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound _sound in sounds) {
+            if (_sound.clip) {
+                if (_sound.custom) {
+                    Debug.LogWarning("An audio source and a custom object were added. If will ignore the custom object instead.");
+                }
+
+                _sound.setSource(gameObject.AddComponent<AudioSource>());
+            } else if(_sound.custom) {
+                _sound.setCustomSource();
+            } else {
+                Debug.LogWarning("An audio source for '" + name + "' was not loaded because both gameObject and clip are not set. Setup one of them if you want settings to take effect.");
+            }
+        }
+    }
+
+    public bool isMasterSoundMuted() {
+        return MuteMasterSound;
+    }
+
+    public void setMasterSoundMuted(bool status) {
+        MuteMasterSound = status;
+
+        foreach(Sound _sound in sounds) {
+            _sound.setMute(_sound.isMuted());
+        }
+    }
+
+    public float getMasterSoundVolume() {
+        return MasterSoundVolume;
+    }
+
+    public void setMasterSoundVolume(float _volume) {
+        if (_volume < .0f) {
+            _volume = .0f;
+        } else if (_volume > 1f) {
+            _volume = 1f;
+        }
+
+        MasterSoundVolume = _volume;
+    }
+
+    public bool isMusicSoundMuted() {
+        return MuteMusicSound;
+    }
+
+    public void setMusicSoundMuted(bool status) {
+        MuteMusicSound = status;
+
+        foreach (Sound _sound in sounds) {
+            if (_sound.getType() == SoundType.Music) {
+                _sound.setMute(_sound.isMuted());
+            }
+        }
+    }
+
+    public float getMusicSoundVolume() {
+        return MusicSoundVolume;
+    }
+
+    public void setMusicSoundVolume(float _volume) {
+        if (_volume < .0f) {
+            _volume = .0f;
+        } else if (_volume > 1f) {
+            _volume = 1f;
+        }
+
+        MusicSoundVolume = _volume;
+    }
+
+    public bool isSFXSoundMuted() {
+        return MuteSFXSound;
+    }
+
+    public void setSFXSoundMuted(bool status) {
+        MuteSFXSound = status;
+
+        foreach (Sound _sound in sounds) {
+            if (_sound.getType() == SoundType.SFX) {
+                _sound.setMute(_sound.isMuted());
+            }
+        }
+    }
+
+    public float getSFXSoundVolume() {
+        return SFXSoundVolume;
+    }
+
+    public void setSFXSoundVolume(float _volume) {
+        if (_volume < .0f) {
+            _volume = .0f;
+        } else if (_volume > 1f) {
+            _volume = 1f;
+        }
+
+        SFXSoundVolume = _volume;
+    }
+
+    public Sound getSound(string name) {
+        return Array.Find(sounds, sound => sound.name == name);
+    }
+
+    public void play(string name) {
+        try {
+            foreach (Sound _sound in sounds) {
+                if (_sound.name == name) {
+                    _sound.playSound();
+                }
+            }
+        } catch (NullReferenceException) {
+            Debug.LogWarning("The sound with the name '" + name + "' does not exist in our list. Make sure you register the sound.");
+            return;
+        }
+    }
+
+    public void mute(string name, bool status) {
+        try {
+            foreach (Sound _sound in sounds) {
+                if (_sound.name == name) {
+                    _sound.setMute(status);
+                }
+            }
+        } catch (NullReferenceException) {
+            Debug.LogWarning("The sound with the name '" + name + "' does not exist in our list. Make sure you register the sound.");
+            return;
+        }
+    }
+
+    public void stop(string name) {
+        try {
+            foreach (Sound _sound in sounds) {
+                if (_sound.name == name) {
+                    _sound.stopSound();
+                }
+            }
+        } catch (NullReferenceException) {
+            Debug.LogWarning("The sound with the name '" + name + "' does not exist in our list. Make sure you register the sound.");
+            return;
+        }
+    }
+
+    public void setVolume(string name, float volume) {
+        try {
+            foreach (Sound _sound in sounds) {
+                if (_sound.name == name) {
+                    _sound.setVolume(volume);
+                }
+            }
+        } catch (NullReferenceException) {
+            Debug.LogWarning("The sound with the name '" + name + "' does not exist in our list. Make sure you register the sound.");
+            return;
+        }
+    }
+}
